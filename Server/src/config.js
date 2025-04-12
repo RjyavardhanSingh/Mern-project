2e7b615db7e743111fee0eae954ec389094a238a
RajyavardhanSingh <rajyavardhansing2003@gmail.com>
2025-04-12 13:49:25 +0530

    first commit

--- Server/src/config.js
+++ Server/src/config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI ;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 module.exports = {MONGO_URI, PORT};
